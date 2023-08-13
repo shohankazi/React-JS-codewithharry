359c745e168b58d250069405d845b5f7e9baa470
shohankazi <shohankazi012@gmail.com>
2023-08-13 11:10:34 +0600

    Chapter-6 Practise Set 1 complete

---
+++ JS Practise Set/Chapter-6.js
@@ -0,0 +1,11 @@
+let age = prompt("Enter your age")
+age = Number.parseInt(age)
+
+const canDrive = (age) =>{
+    return age >= 18 ? true:false
+}
+if (canDrive(age)) {
+    alert("yes you can drive")
+} else {
+    alert("You can't drive")
+}
